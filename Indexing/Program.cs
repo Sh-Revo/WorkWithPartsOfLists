362ca588ffbaec32942d359a35978f0f8cc721b8
Sh-Revo <shandov@i.ua>
2022-04-16 22:07:50 +0300

    add Equals and Contains methods

--- Indexing/Program.cs
+++ Indexing/Program.cs
@@ -4,22 +4,6 @@ using System.Collections.Generic;
 
 namespace Indexing
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var list = new MyList<int>();
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-
-            for (int i = 0; i <= list.Count; i++)
-            {
-                Console.WriteLine(list[i]);
-            }
-        }
-    }
-
     public class MyList<T> : IEnumerable<T>
     {
         T[] array;
@@ -71,5 +55,46 @@ namespace Indexing
         }
 
         public int Count { get { return count; } }
+
+        public bool Contains(T element)
+        {
+            foreach (var e in this)
+            {
+                if (e.Equals(element)) return true;
+            }
+            return false;
+        }
+    }
+
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Point)) return false;
+            var p = obj as Point;
+            return X == p.X && Y == p.Y;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var list = new MyList<int>();
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
+
+            //for (int i = 0; i <= list.Count; i++)
+            //{
+            //    Console.WriteLine(list[i]);
+            //}
+            var pList = new List<Point>();
+            pList.Add(new Point { X = 1, Y = 1 });
+            Console.WriteLine(pList.Contains(new Point { X = 1, Y = 1 }));
+        }
     }
 }
